0dda0c6404880f9f4daa8f1e8c72845b79cdcb27
Leon <jq153387@gmail.com>
2020-04-30 15:20:38 +0800

    update output lang post

--- _main/index.md
+++ _main/index.md
@@ -2,6 +2,16 @@
 layout: default
 title: 與眾不同的新創孵化
 description: A Different Breed of Startup Incubator
+locales:
+    zh-tw:
+        title: 與眾不同的新創孵化
+        description: 與眾不同的新創孵化
+    zh-cn:
+        title: 與眾不同的新創孵化
+        description: 與眾不同的新創孵化
+    en:
+        title: A Different Breed of Startup Incubator
+        description: A Different Breed of Startup Incubator
 ---
 
 <!---
@@ -20,7 +30,6 @@ description: A Different Breed of Startup Incubator
 
 我們追求激進式的透明，讓公司裡最好的想法永遠勝出。在此[中央資料庫中]({{ site.github.repository_url }})，您會找到所有需要瞭解我們團隊的資訊，徹底了解團隊運作方式。我們員工內部使用的所有信息，您可以在這裡檢閱。我們鼓勵您利用這網站去了解我們。為了追求精英式思維，我們樂於分享我們團隊的管理、經營決策，激進的透明度促使事情的真相浮出表面，需要極度的真實和極度的透明制度。
 
-
 <br>
 
 ## 唯才是用
@@ -35,19 +44,20 @@ description: A Different Breed of Startup Incubator
 
 幫我們支持台灣開源軟體，新創，及工程師社團：
 
-* [台灣新創工作機會社團 :rocket:](https://021tw.github.io/021tw.github.io/){:target="_blank"}
-* [台灣全端工程師社團 :star:](https://stacktw.github.io/stacktw.github.io/){:target="_blank"}
-* [台灣遠端軟體工程師徵才 :palm_tree:](https://www.linkedin.com/groups/10525064/){:target="_blank"}
-	* [LinkedIn](https://www.linkedin.com/groups/10525064/){:target="_blank"}
-	* [Facebook](https://www.facebook.com/groups/489046765360247/){:target="_blank"}
+-   [台灣新創工作機會社團 :rocket:](https://021tw.github.io/021tw.github.io/){:target="\_blank"}
+-   [台灣全端工程師社團 :star:](https://stacktw.github.io/stacktw.github.io/){:target="\_blank"}
+-   [台灣遠端軟體工程師徵才 :palm_tree:](https://www.linkedin.com/groups/10525064/){:target="\_blank"}
+    _ [LinkedIn](https://www.linkedin.com/groups/10525064/){:target="\_blank"}
+    _ [Facebook](https://www.facebook.com/groups/489046765360247/){:target="\_blank"}
 
 <br>
 
 ## 歡迎大家的建議
 
 我們歡迎各位的建議：
-* 請由 [git pull request]({{ site.github.repository_url }}/pull/new/master)建議文件更改。
-* 請由 [git issue]({{ site.github.repository_url }}/issues/new)投入您的疑問。
+
+-   請由 [git pull request]({{ site.github.repository_url }}/pull/new/master)建議文件更改。
+-   請由 [git issue]({{ site.github.repository_url }}/issues/new)投入您的疑問。
 
 <br>
 
@@ -67,7 +77,6 @@ description: A Different Breed of Startup Incubator
 
 在此[中央资料库中]({{ site.github.repository_url }})，您会发现所有需要了解我们团队的资讯，彻底了解团队运作方式。我们员工内部使用的所有信息，您可以在这里检阅。我们鼓励您利用这网站去了解我们。为了追求精英式的思维，我们乐于分享我们团队的管理，经营决策，激进的替代做法的真相浮出表面，需要极度的真实和极度的透明制度。
 
-
 <br>
 
 ## 唯才是用
@@ -82,17 +91,18 @@ description: A Different Breed of Startup Incubator
 
 帮我们支持台湾开源软体，新创，及工程师社团：
 
-* [台湾新创工作机会社团 :rocket:](https://021tw.github.io/021tw.github.io/){:target="_blank"}
-* [台湾全端工程师社团 :star:](https://stacktw.github.io/stacktw.github.io/){:target="_blank"}
-* [远端软体工程师征才 :palm_tree:](https://www.linkedin.com/groups/10532717/){:target="_blank"}
+-   [台湾新创工作机会社团 :rocket:](https://021tw.github.io/021tw.github.io/){:target="\_blank"}
+-   [台湾全端工程师社团 :star:](https://stacktw.github.io/stacktw.github.io/){:target="\_blank"}
+-   [远端软体工程师征才 :palm_tree:](https://www.linkedin.com/groups/10532717/){:target="\_blank"}
 
 <br>
 
 ## 欢迎大家的建议
 
 我们欢迎各位的建议：
-* 请由[git pull request]({{ site.github.repository_url }}/pull/new/master)建议文件更改。
-* 请由[git issue]({{ site.github.repository_url }}/issues/new)提出您的疑问。
+
+-   请由[git pull request]({{ site.github.repository_url }}/pull/new/master)建议文件更改。
+-   请由[git issue]({{ site.github.repository_url }}/issues/new)提出您的疑问。
 
 <br>
 
@@ -104,13 +114,13 @@ description: A Different Breed of Startup Incubator
 
 ## All Remote Team, Incubate New Ideas
 
-We incubate, accelerate, and implement ideas that our team create internally. 
+We incubate, accelerate, and implement ideas that our team create internally.
 
 <br>
 
-## Radical Transparency 
+## Radical Transparency
 
-We believe in radical transparency, where the best ideas win. As such, how we run the company is [open sourced to the world]({{ site.github.repository_url }}). In this central repository, you will find all the information you need to understand what it is like to work here. All the information available to our employees is also available to you. We encourage you to learn more about us here at this depository. 
+We believe in radical transparency, where the best ideas win. As such, how we run the company is [open sourced to the world]({{ site.github.repository_url }}). In this central repository, you will find all the information you need to understand what it is like to work here. All the information available to our employees is also available to you. We encourage you to learn more about us here at this depository.
 
 <br>
 
@@ -126,19 +136,19 @@ We want to work with talented individuals and are **always actively hiring**.
 
 Help us support the open source, startup, developer communites in Taiwan by following:
 
-* [Startup Jobs Taiwan :rocket:](https://021tw.github.io/){:target="_blank"}
-* [Full Stack Developers Taiwan :star:](https://stacktw.github.io/){:target="_blank"}
-* [Remote Developers Jobs :palm_tree:](https://www.linkedin.com/groups/10525064/){:target="_blank"}
-	* [LinkedIn](https://www.linkedin.com/groups/10525064/){:target="_blank"}
-	* [Facebook](https://www.facebook.com/groups/489046765360247/){:target="_blank"}
+-   [Startup Jobs Taiwan :rocket:](https://021tw.github.io/){:target="\_blank"}
+-   [Full Stack Developers Taiwan :star:](https://stacktw.github.io/){:target="\_blank"}
+-   [Remote Developers Jobs :palm_tree:](https://www.linkedin.com/groups/10525064/){:target="\_blank"}
+    _ [LinkedIn](https://www.linkedin.com/groups/10525064/){:target="\_blank"}
+    _ [Facebook](https://www.facebook.com/groups/489046765360247/){:target="\_blank"}
 
 <br>
 
 ## Open Feedback
 
 We welcome feedback.
-* Please make a [pull request]({{ site.github.repository_url }}/pull/new/master) to suggest improvements or add clarification.
-* Please create a [issue]({{ site.github.repository_url }}/issues/new) to ask questions.
+
+-   Please make a [pull request]({{ site.github.repository_url }}/pull/new/master) to suggest improvements or add clarification.
+-   Please create a [issue]({{ site.github.repository_url }}/issues/new) to ask questions.
 
 <br>
-
--- ie8n copy.py
+++ ie8n copy.py
@@ -1,76 +1,164 @@
 from bs4 import BeautifulSoup
-import pickle
-f = open("./_main/index.md", "r")
-soup = BeautifulSoup(f, 'html.parser')
-# html = soup.find(id='some_id')
-# soup = BeautifulSoup(f, features="html.parser")
-
-##print(atag.find_next_siblings())
-#print(soup.find(lang))
+import frontmatter
+import yaml
+filepath = "./_main/index.md"
 setlang = ["en", "zh-tw", "zh-cn"]
+tag = "a"
+attrname = "name"
 
-print(soup.find_all('a'))
-langname = []
-for lang in soup.find_all('a'):
-    print(lang.get('name'))
-    for name in setlang:
-        if lang.get('name') == name:
-            langname.append(name)
+# ducyaml = """
+# ---
+# layout: default
+# title: Welcome to my web!
+# lang: zh-cn
+# lang-ref: welcome-to-my-web
+# ---
+# """
+# data = exec(ducyaml)
+# with open(ducyaml, "r") as stream:
+# data = yaml.load(ducyaml)
+# dict = {'name': 'ngy', 'age': 23, 'height': 180}
+# print(dict)
+# print(dict["nams"])
+# data = list(yaml.load_all(ducyaml))[0]
+# if key in mydict:
+#     value = mydict[key]
+# print(data['filepath'])
+# for s in data:
 
-print(langname)
-article_lang = {}
-for value in langname:
-    atag = soup.find("a", {"name": value})
-    article = []
-    for elm in atag.find_next_siblings():
-        article.append(elm)
-        article.append(elm.next_sibling)
+#     print(s)
+
+
+def rfileDate(filepath, setlang, tag, attrname):
+    """
+    讀取目錄資料夾檔案，使用標籤參數拆分
+    
+    Args:
+        filepath: 檔案目錄
+        setlang: 檔案語言list[]
+        tag: 標籤 a
+        attrname: 指定查詢標籤參數 name
+    Returns:
+        使用查詢到的標籤參數值返回dict
+        {   
+            'zh-tw': [string, string],
+            'zh-cn': [string, string],
+            'zh-tw': [string, string]
+        }
+    """
+    f = open(filepath, "r")
+    soup = BeautifulSoup(f, 'html.parser')
+
+    metadata = frontmatter.load(filepath).metadata
+    print(metadata)
+    # print(post.keys(), 'keys')
+    # 內容標籤值與設定值(語系)是否存在
+    langname = []
+    for lang in soup.find_all(tag):
+        for name in setlang:
+            if lang.get(attrname) == name:
+                langname.append(name)
+
+    # print(langname)
+    article_lang = {}
+    # print(document_data)
+    for value in langname:
+        atag = soup.find(tag, {attrname: value})
+        article = []
+
+        #print(document_data)
+        if "locales" in metadata:
+            if value in metadata['locales']:
+                post_title = metadata['locales'][value]['title']
+                post_description = metadata['locales'][value]['description']
+            else:
+                ##使用default lang_mate
+                post_title = metadata['title']
+                post_description = metadata['description']
+        else:
+            ##使用default lang_mate
+            post_title = metadata['title']
+            post_description = metadata['description']
+
+        lang_metadata = [
+            '---', 'layout: ' + metadata['layout'], 'title: ' + post_title,
+            'lang: ' + value, 'description: ' + post_description,
+            'lang-ref: welcome-to-my-web', '---\n\n'
+        ]
+        # data = list(yaml.load_all(f, Loader=yaml.FullLoader))
+        story = '\n'.join(lang_metadata)
+        # print(data)
+        article.append(story)
+        ##第一的元素的之後的文字
+        article.append(atag.next_sibling.strip() + "\n\n")
+        atag_next_siblings = atag.find_next_siblings()
+        # if elm.get(attrname) == value:
+        now_lag_index = langname.index(value)
+        # print(now_lag_index)
+        # print(langname[now_lag_index + 1])
+        if now_lag_index + 1 < len(langname):
+            # print(now_lag_index + 1)
+            next_element = soup.find(tag,
+                                     {attrname: langname[now_lag_index + 1]})
+            if next_element != None:
+                next_element_index = atag_next_siblings.index(next_element)
+                atag_split = atag_next_siblings[0:next_element_index]
+        else:
+            atag_split = atag_next_siblings
+            # print(atag_split)
+        # print(atag_next_siblings)
+        for atag_elm in atag_split:
+            article.append(atag_elm)
+            article.append(atag_elm.next_sibling)
+            pass
+
         #print(elm.next_sibling)
-    # print(artice)
-    article_lang[value] = article
+        # print(artice)
+        article_lang[value] = article
+    pass
+
+    return article_lang
+
+
+article_lang = rfileDate(filepath, setlang, tag, attrname)
+
 # print(article_lang)
 print("============================================================>")
-with open("./sss.md", "w") as fp:
+
+path = "/index.md"
+fname = "_pages_"
+
+
+def creatFiles(article_lang, path, fname):
+    """
+    Args:
+        filepath:  標籤參數如語系區分的資料
+            {   
+                'zh-tw': [string, string],
+                'zh-cn': [string, string],
+                'zh-tw': [string, string]
+            }
+        path: 輸入相對檔案路徑 "/index.md"
+        name: 輸出的資料夾主別名目綠 "_pages_"
+   
+    """
     for key in article_lang:
+        with open("./" + fname + key + path, "w") as fp:
+            for item in article_lang[key]:
+                # print(item)
+                #pickle.dump(str(item), fp)
+                fp.write(str(item))
 
-        for item in article_lang[key]:
-            print(item)
-            #pickle.dump(str(item), fp)
-            fp.write(str(item))
+
+creatFiles(article_lang, path, fname)
+print("end============================================================>")
 # print(soup.prettify())
+# 建立的目錄路徑
+# folderpath = "/home/gtwang/my_folder"
 
-import os
-
-# 指定要查詢的路徑
-
-yourPath = './'
-
-# # 列出指定路徑底下所有檔案(包含資料夾)
-
-# allFileList = os.listdir(yourPath)
-
-# print(allFileList)
-
-# # 逐一查詢檔案清單
-
-# for file in allFileList:
-
-#     #   這邊也可以視情況，做檔案的操作(複製、讀取...等)
-
-#     #   使用isdir檢查是否為目錄
-
-#     #   使用join的方式把路徑與檔案名稱串起來(等同filePath+fileName)
-
-#     if os.path.isdir(os.path.join(yourPath, file)):
-
-#         print("I'm a directory: " + file)
-
-# #   使用isfile判斷是否為檔案
-
-#     elif os.path.isfile(yourPath + file):
-
-#         print(file)
-
-#     else:
-
-#         print('OH MY GOD !!')
+# # 使用 try 建立目錄
+# try:
+#     os.makedirs(folderpath)
+# # 檔案已存在的例外處理
+# except FileExistsError:
+#     print("檔案已存在。")
